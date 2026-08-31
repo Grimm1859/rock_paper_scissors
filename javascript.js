d3750211e3740783b2d189d6ea125771353fbf72
Grimm1859 <nguyentrungkien12092@gmail.com>
2026-08-31 15:48:18 +0700

    Finish the very first version of the game. Add all of the neccesary functions. Known issue: the game will continue even if player's choice is invalid. It will instead calculate with the previous value instead of null.

--- javascript.js
+++ javascript.js
@@ -1,26 +1,76 @@
-function getRandomInt(){
-   return Math.floor(Math.random() * 3 + 1); 
-}
+let computerNumber;
+let computerChoice;
+let humanChoice;
+let humanNumber;
+let roundNumber = 1;
+let humanScore = 0;
+let computerScore = 0;
 
-let randomNumber = getRandomInt();
+function getComputerNumber(){
+   computerNumber =  Math.floor(Math.random() * 3 + 1); 
+}
 
 function getComputerChoice(){
-    switch(randomNumber){
-        case 1: 
-            return "rock";
-            break;
-        case 2:
-            return "paper";
-            break;
-        default:
-            return "scissors"; 
-    }
+switch (computerNumber){
+    case 1:
+        computerChoice = "rock"
+        break;
+    case 2:
+        computerChoice = "paper"
+        break;
+    case 3:
+        computerChoice = "scissors"
+        break;
 }
-
-console.log(getComputerChoice());
+}
 
 function getHumanChoice(){
-    return prompt("What do you choose?")
+    humanChoice = prompt("What do you choose?")
 }
 
-console.log(getHumanChoice());
+function getHumanNumber(){
+switch (humanChoice){
+    case "rock":
+        humanNumber = 1
+        break;
+    case "paper":
+        humanNumber = 2
+        break;
+    case "scissors":
+        humanNumber = 3
+        break;
+    default:
+        console.log("Choice not valid!")
+}
+}
+
+function roundStart(){
+    getComputerNumber();
+    getComputerChoice();
+    getHumanChoice();
+    getHumanNumber();
+    console.log("Computer's choice is " + computerChoice + ", Your choice is " + humanChoice );
+    if(humanNumber - computerNumber == 1 || humanNumber - computerNumber ==-2 ){
+        console.log("You won this round!")
+        humanScore = ++humanScore
+    } else if ( humanNumber == computerNumber) {
+        console.log("Tie!")
+    } else {
+        console.log("You lost this round!")
+        computerScore = ++computerScore
+    }
+    nextRound();
+}
+
+roundStart();
+
+function nextRound(){
+    console.log("Current score is: You: " + humanScore + " Computer: " + computerScore)
+    if(humanScore == 5){
+        console.log("You've won the game. Congrats!")
+    } else if (computerScore == 5){
+        console.log("You've lost the game. Try again?")
+    } else {
+        roundStart();
+    }
+}
